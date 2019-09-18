261e14901da9b7c93791cc2f176cb204d7af9289
Steen RH <steen.rickmer@gmx.de>
2019-09-18 23:55:38 +0200

    optimized vars, fixed errors

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ var table = document.getElementsByClassName("tableizer-table")[0];
 var sp = table.style;
 var nowB = document.getElementById("nowB");
 var playB = document.getElementById("playB");
-var tableB = document.getElementById("toggleTable");
+var tableB = document.getElementById("tableB");
 
 var identificationRow = 1;
+var linesPerDayOfWeek = 14;
 
 var fillColor = "#b40000";
 var animating = false;
@@ -17,7 +18,16 @@ var date = new Date();
 var fach = 0;
 var found = 0;
 
-// table coloring stuff
+var roomNums = [];
+for (var i = 0, col; (col = table.rows[identificationRow].cells[i]); i++) {
+	if (col.innerHTML != "&nbsp;" &&
+		col.innerHTML != " " &&
+		col.innerHTML != "" &&
+		col.innerHTML != "Raumnr.") {
+		roomNums.push(col.innerHTML);
+	}
+}
+
 
 
 updateT();
@@ -47,66 +57,13 @@ function updateT() {
 
 	for (var i = 0; i < radios.length; i++) {
 		if (radios[i].checked) {
-			day = radios[i].value * 14;
+			day = radios[i].value * linesPerDayOfWeek;
 			//console.log(day);
 			break;
 		}
 	}
 
-	var roomNums = [
-		"3/0/32",
-		"3/0/31",
-		"3/1/2",
-		"3/1/3",
-		"3/1/4",
-		"3/1/6",
-		"3/1/9",
-		"3/1/10",
-		"3/1/15",
-		"3/1/20",
-		"3/1/21",
-		"3/1/22",
-		"3/1/23",
-		"3/1/24",
-		"3/2/2",
-		"3/2/3",
-		"3/2/4",
-		"3/2/8",
-		"3/2/11",
-		"3/2/16",
-		"3/2/17",
-		"3/2/21",
-		"3/2/24",
-		"3/2/23",
-		"3/2/22",
-		"3/2/26",
-		"3/2/27",
-		"3/3/1",
-		"3/3/2",
-		"3/3/6",
-		"3/3/8",
-		"3/3/11",
-		"3/3/13",
-		"3/3/10",
-		"5/0/6",
-		"5/0/7",
-		"5/0/8",
-		"5/1/1",
-		"5/1/2",
-		"5/1/6",
-		"5/1/5",
-		"4/1/14",
-		"4/1/26",
-		"4/1/24",
-		"4/1/20",
-		"4/1/19",
-		"4/1/15",
-		"4/1/25",
-		"4/0/1",
-		"4/0/2",
-		"4/0/3", 
-		"6/0/14",
-		"6/0/15"];
+	
 	highlAll();
 
 	function highlAll() {
@@ -120,18 +77,16 @@ function updateT() {
 
 	function highlightRoom(room) {
 		for (var i = 0, col; (col = table.rows[identificationRow].cells[i]); i++) {
-			if (col.innerHTML.toLowerCase() == room) {
+			if (document.getElementById(room) != null && col.innerHTML.toLowerCase() == room) {
 				for (var j = 0; j <= 13; j++) {
 					table.rows[3 + day + j].cells[i].style.background = "none";
 				}
 				document.getElementById(room).style.fill = "rgba(0,0,0,0.01)";
 
 				var current = table.rows[3 + fach + day].cells[i];
-				if (
-					current.innerHTML != "&nbsp;" &&
+				if (current.innerHTML != "&nbsp;" &&
 					current.innerHTML != " " &&
-					current.innerHTML != ""
-				) {
+					current.innerHTML != "") {
 					current.style.background = "cyan";
 					document.getElementById(room).style.fill = fillColor;
 				}
@@ -220,23 +175,23 @@ function animate(){
   setTimeout(animate,5);
 }
 
-document.onkeydown = function(e) {
+document.onkeydown = function(pressedKey) {
   	var value = parseInt(slider.value);
-    switch (e.keyCode) {
+    switch (pressedKey.keyCode) {
         case 37: //left
-        	e.preventDefault();
+        	pressedKey.preventDefault();
         	slider.value = value - 60;
             break;
         case 39: //right
-        	e.preventDefault();
+        	pressedKey.preventDefault();
 			slider.value = value + 60;
             break;    
         case 38: //up
-        	e.preventDefault();
+        	pressedKey.preventDefault();
         	newDay("prev");
             break;
         case 40: //down
-        	e.preventDefault();
+        	pressedKey.preventDefault();
         	newDay();
             break;
     }
